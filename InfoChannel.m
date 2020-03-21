d89c271af7f7153fee5c33dc6669d07497e661a4
sjinglong <1368351931@qq.com>
2020-03-21 17:45:39 +0800

    添加了陈逸的算法

--- InfoChannel.m
+++ InfoChannel.m
@@ -7,7 +7,7 @@ classdef InfoChannel
             obj.dataBoardHandle_ = dataBoardHandle;
         end
         function out = gaussian(obj, in)
-            if obj.dataBoardHandle_.modulationType ~= "16QAM"
+            if isreal(in)
                 out = in + normrnd(0, sqrt(obj.dataBoardHandle_.noisePower / 2), length(in), 1);
             else
                 out = in + normrnd(0, sqrt(obj.dataBoardHandle_.noisePower / 2), length(in), 1) + ...
